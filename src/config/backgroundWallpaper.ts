9efae823fd62833fdbb3f49c01b9538c7c6a17a6
esoter <enzhaowang@outlook.com>
2026-09-26 02:59:25 +0800

    Update background wallpaper URLs and settings

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -18,10 +18,10 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	 * 所以建议使用自己的图片的时候命名为其他名称，不要使用d1-d6，m1-m6这些名称
 	 *
 	 * 如果只使用一张图片或者使用随机图API，推荐直接使用字符串格式：
-	 * desktop: "https://t.alcy.cc/pc",   // 随机图API
+	 * desktop: "https://t.alcy.cc/ycy",   // 随机图API
 	 * desktop: "assets/images/DesktopWallpaper/d1.avif", // 单张图片
 	 *
-	 * mobile: "https://t.alcy.cc/mp", // 随机图API
+	 * mobile: "https://t.alcy.cc/ycy", // 随机图API
 	 * mobile: "assets/images/MobileWallpaper/m1.avif", // 单张图片
 	 *
 	 * 支持配置多张图片（数组），每次刷新页面随机显示一张：
@@ -38,24 +38,17 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	src: {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
-		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
-		],
+		//desktop: [
+		//	"assets/images/DesktopWallpaper/d1.avif",
+		//	"assets/images/DesktopWallpaper/d2.avif",
+		//	"assets/images/DesktopWallpaper/d3.avif",
+		//	"assets/images/DesktopWallpaper/d4.avif",
+		//	"assets/images/DesktopWallpaper/d5.avif",
+		//	"assets/images/DesktopWallpaper/d6.avif",
+		//],
+		desktop: "https://t.alcy.cc/ycy",
 		// 移动背景图片（支持单张或多张随机）
-		// mobile: "assets/images/MobileWallpaper/m1.avif",
-		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
-		],
+		mobile: "https://t.alcy.cc/ycy",
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
 		// 支持远程视频URL，本地视频请放在 public/assets/videos/ 目录下
@@ -73,7 +66,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "hello",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
@@ -100,7 +93,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 				pauseTime: 2000,
 			},
 			// 是否显示标题下方的链接图标
-			linksEnable: true,
+			linksEnable: false,
 			// 首页横幅标题下方的链接图标（可选，支持 showName 显示文字）
 			// 图标支持 Iconify 格式：fa7-brands:github、fa7-solid:envelope、mdi:rss 等
 			links: [
